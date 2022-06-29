4d51e48af16e70fbad474d005e2f0adb0388730b
Kamran <jameelk809@gmail.com>
2022-06-29 23:46:32 +0530

    importing model & creating a resource

--- notable_django/urls.py
+++ notable_django/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+from api.resources import NoteResource
+
+note_resource = NoteResource()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('api/', include(note_resource.urls)),
 ]
